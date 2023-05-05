77f37fdfa146b3cf0651e3951ff01956bc4cdf01
Elena Koroleva <lenatonic@yandex.ru>
2023-05-05 16:34:15 +0300

    fix: править codStyle testLike

--- src/test/java/ru/yandex/practicum/filmorate/integrationTests/LikeDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/integrationTests/LikeDbStorageTest.java
@@ -13,7 +13,6 @@ import ru.yandex.practicum.filmorate.storage.film.FilmDbStorage;
 import ru.yandex.practicum.filmorate.storage.like.LikeDbStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserDbStorage;
 
-import javax.validation.constraints.Max;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.List;
@@ -39,21 +38,21 @@ public class LikeDbStorageTest {
                 .name("user1")
                 .email("user1@test.com")
                 .login("logUser1")
-                .birthday(LocalDate.of(2001, 01, 01))
+                .birthday(LocalDate.of(2001, 1, 1))
                 .build();
 
         user2 = User.builder()
                 .name("user2")
                 .email("user2@test.com")
                 .login("logUser2")
-                .birthday(LocalDate.of(2002, 02, 02))
+                .birthday(LocalDate.of(2002, 2, 2))
                 .build();
 
         filmFirst = Film.builder()
                 .name("Бегущий по лезвию бритвы")
                 .description("Классика киберпанка по мотивам романа Филипа Дика.")
                 .duration(117)
-                .releaseDate(LocalDate.of(1982, 06, 25))
+                .releaseDate(LocalDate.of(1982, 6, 25))
                 .mpa(new Mpa(4))
                 .build();
 
@@ -61,7 +60,7 @@ public class LikeDbStorageTest {
                 .name("Автостопом по галактике")
                 .description("Уморительная комедия по роману Дугласа Адамса.")
                 .duration(109)
-                .releaseDate(LocalDate.of(2005, 04, 20))
+                .releaseDate(LocalDate.of(2005, 4, 20))
                 .mpa(new Mpa(2))
                 .genres(new HashSet<>())
                 .build();
@@ -89,6 +88,5 @@ public class LikeDbStorageTest {
         assertThat(likeFilm12.get(0)).isEqualTo(1);
         assertThat(likeFilm12.get(1)).isEqualTo(2);
         assertThat(likeFilmAfterDelete.get(0)).isEqualTo(1);
-
     }
 }
